9a9a6435c62ffa708a2f32e9f880957ba5077913
joshuacirilo <u175692795@byui.edu>
2026-02-07 15:00:19 -0600

    add: message in the session

--- routes/static.js
+++ routes/static.js
@@ -2,6 +2,11 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+// Favicon route to avoid 404s from /favicon.ico
+router.get("/favicon.ico", (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'images', 'site', 'favicon-32x32.png'))
+})
+
 // Static Routes
 // Set up "public" folder / subfolders for static files
 router.use(express.static(path.join(__dirname, '..', 'public')));
@@ -10,6 +15,5 @@ router.use("/js", express.static(path.join(__dirname, '..', 'public', 'js')));
 router.use("/images", express.static(path.join(__dirname, '..', 'public', 'images')));
 
 module.exports = router;
-
 
 
--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 /* ***********************
  * Require Statements
  *************************/
+const session = require("express-session")
+const pool = require('./database/')
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities/")
@@ -14,6 +16,29 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const { title } = require("process")
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+
 
 /* ***********************
  * View Engine and Templates
